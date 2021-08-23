86094f6bc884c720f3b92b96f803e42ad130efa2
adi <52442230+adi-lazaro@users.noreply.github.com>
2021-08-23 18:02:15 +0530

    BFS: Migrate over to the new structure (2)

--- style.php
+++ style.php
@@ -1,31 +1,31 @@
 
 <!-- Base -->
-<link rel="stylesheet" type="text/css" href="css/1_normalize.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/2_base.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/3_grid.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/4_helper.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/5_stylescape.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/1_normalize.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/2_base.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/3_grid.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/4_helper.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/5_stylescape.css<?php echo $ver ?>">
 
 <!-- Modules -->
-<link rel="stylesheet" type="text/css" href="css/modules/header.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/modules/video-embed.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/modules/modal-box.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/modules/lazaro-signature.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/modules/header.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/modules/video-embed.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/modules/modal-box.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/modules/lazaro-signature.css<?php echo $ver ?>">
 
 <!-- Pages + Sections + Modals -->
-<link rel="stylesheet" type="text/css" href="css/pages/page.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/modal/modal-menu.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/sample-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/navigation-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/header-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/menu-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/landing-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/intro-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/gallery-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/programs-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/articles-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/brochure-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/booking-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/team-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/post-section.css<?php echo $ver ?>">
-<link rel="stylesheet" type="text/css" href="css/pages/section/footer-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/page.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/modal/modal-menu.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/sample-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/navigation-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/header-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/menu-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/landing-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/intro-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/gallery-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/programs-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/articles-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/brochure-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/booking-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/team-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/post-section.css<?php echo $ver ?>">
+<link rel="stylesheet" type="text/css" href="/css/pages/section/footer-section.css<?php echo $ver ?>">
