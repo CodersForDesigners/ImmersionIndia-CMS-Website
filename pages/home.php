b77b6b1a83f447f7f50a19a175caee150bb42e68
adi <52442230+adi-lazaro@users.noreply.github.com>
2021-08-23 18:03:16 +0530

    Travel Programs, Virtual Series: Add these post types, modularize the form

--- pages/home.php
+++ pages/home.php
@@ -60,6 +60,39 @@ foreach ( $programs as $program ) {
 }
 
 
+
+/*
+ |
+ | Travel Programs
+ |
+ */
+$travelPrograms = CMS::getPostsOf( 'travel_programs' );
+foreach ( $travelPrograms as $travelProgram ) {
+	$travelProgram->set( 'title', $travelProgram->get( 'post_title' ) );
+	$travelProgram->set( 'type', 'Travel' );
+	$image = $travelProgram->get( 'image' );
+	$travelProgram->set( 'image', $image[ 'sizes' ][ 'small' ] ?? $image[ 'sizes' ][ 'thumbnail' ] ?? $image[ 'sizes' ][ 'medium' ] ?? $image[ 'url' ] ?? '/media/fallback-image.png' );
+	$travelProgram->set( 'attachment', $travelProgram->get( 'details_pdf' )[ 'url' ] ?? '#' );
+}
+
+
+
+/*
+ |
+ | Virtual Series
+ |
+ */
+$virtualSeries = CMS::getPostsOf( 'virtual_series' );
+foreach ( $virtualSeries as $series ) {
+	$series->set( 'title', $series->get( 'post_title' ) );
+	$series->set( 'type', 'Virtual' );
+	$image = $series->get( 'image' );
+	$series->set( 'image', $image[ 'sizes' ][ 'small' ] ?? $image[ 'sizes' ][ 'thumbnail' ] ?? $image[ 'sizes' ][ 'medium' ] ?? $image[ 'url' ] ?? '/media/fallback-image.png' );
+	$series->set( 'attachment', $series->get( 'details_pdf' )[ 'url' ] ?? '#' );
+}
+
+
+
 /*
  |
  | Posts
@@ -108,6 +141,16 @@ foreach ( $members as $member ) {
 	$member->set( 'name', $member->get( 'post_title' ) );
 	$member->set( 'image', $member->get( 'image' )[ 'sizes' ][ 'small' ] ?? $thumbnailFallbackImage );
 }
+
+
+
+/*
+ |
+ | UI Components
+ |
+ */
+require_once __ROOT__ . '/pages/sections/program-booking.php';
+
 
 
 // Pull in the header section
@@ -428,88 +471,10 @@ require_once __ROOT__ . '/inc/header.php';
 </section>
 <!-- END: Quote Section -->
 
-<!-- Booking Section -->
-<section class="booking-section space-100-top-bottom fill-neutral-1" id="section-booking" data-section-title="Booking Section" data-section-slug="booking-section">
-	<div class="container">
-		<div class="row">
-			<div class="columns small-12 medium-10 medium-offset-1">
-				<div class="h2 text-uppercase space-min-bottom">Customize Your Program</div>
-				<div class="row">
-					<div class="underline columns small-4 medium-3 large-2 space-min-bottom"><span class="fill-pink"></span></div>
-					<div class="form columns small-12 large-10">
-						<!-- Form -->
-						<form class="row space-50-bottom js_enquiry_form">
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase required">Full Name</span>
-									<input type="text" id="js_form_input_name" class="block" required>
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase required">Email ID</span>
-									<input type="text" id="js_form_input_email" class="block" required>
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase">Phone Number</span>
-									<input type="text" id="js_form_input_phone" class="block">
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase">College/University</span>
-									<input type="text" id="js_form_input_institution" class="block">
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase">Choose a Program</span>
-									<input type="hidden" id="js_form_input_program_id" name="program-id">
-									<select class="block" id="js_form_input_program">
-										<option value="" disabled selected>-- Select Program --</option>
-										<?php foreach ( $programs as $program ) : ?>
-											<option id="<?= $program->get( 'ID' ) ?>" value="<?= $program->get( 'type' ) ?>: <?= $program->get( 'subject' ) ?>"><?= $program->get( 'type' ) ?>: <?= $program->get( 'subject' ) ?> [ <?= $program->get( 'title' ) ?> ]</option>
-										<?php endforeach; ?>
-									</select>
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase">Suggest a Date</span>
-									<input type="date" id="js_form_input_date" class="block">
-								</label>
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<!-- Empty Slot -->
-							</div>
-							<div class="form-row columns small-12 medium-6 space-min-bottom">
-								<label>
-									<span class="label strong text-uppercase invisible">Submit</span>
-									<button class="block fill-pink" type="submit">Submit</button>
-								</label>
-							</div>
-						</form>
-						<!-- END: Form -->
-						<div class="email-action">
-							<a href="mailto:experiences@immersionindia.com" target="_blank" class="block fill-neutral-2 space-50" tabindex="-1">
-								<div class="icon inline-bottom space-25-right">
-									<img class="block" src="../media/icon/icon-email-color.svg<?= $ver ?>">
-								</div>
-								<div class="inline-bottom">
-									<div class="h5 text-neutral-4">or, drop us an email at…</div>
-									<div class="email-id h3 strong text-pink w-400" style="letter-spacing: 0.05rem;"><span class="text-underline">experiences</span><span class="w-500">@</span>immersionindia.com</div>
-								</div>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- END: Booking Section -->
+<?php
+	// Program Booking section
+	BFS\UI\programBooking( $programs )
+?>
 
 <!-- Team Section -->
 <section class="team-section space-100-top-bottom fill-dark" id="section-team" data-section-title="Team Section" data-section-slug="team-section">
---
+++ pages/travel-programs.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once __ROOT__ . '/inc/utils.php';
+require_once __ROOT__ . '/inc/cms.php';
+
+use BFS\Router;
+use BFS\CMS;
+CMS::setupContext();
+
+global $thePost;
+$thePost = CMS::getThisPost();
+
+$featuredImageHeight = CMS::get( 'featured_image_height' );
+$featuredImageStyle = '';
+
+if ( !empty( $featuredImageHeight ) )
+	$featuredImageStyle = 'padding-top: calc( '. $featuredImageHeight . '% - 60px ) !important;';
+
+
+$featuredImageFallback = CMS::get( 'post_featured_fallback_image / sizes / medium' );
+$featuredImage = $thePost->get( 'image' )[ 'sizes' ][ 'large' ] ?: get_the_post_thumbnail_url( $thePost->get( 'ID' ) ) ?: $featuredImageFallback;
+
+$socialImage = CMS::get( 'social_image / sizes / medium' );
+
+$postContent = str_replace( ']]>', ']]&gt;', apply_filters( 'the_content', $thePost->get( 'post_content' ) ) );
+
+
+
+/*
+ |
+ | UI Components
+ |
+ */
+require_once __ROOT__ . '/pages/sections/program-booking.php';
+
+
+
+require_once __ROOT__ . '/inc/header.php';
+
+?>
+
+
+
+<!-- Landing Section -->
+<section class="landing-section fill-dark js_sticky_marker" id="landing-section" data-section-title="Landing Section" data-section-slug="landing-section">
+	<div class="landing-image-bg">
+		<div class="image" style="background-image: url( '<?= $featuredImage ?>' );"></div>
+	</div>
+	<div class="landing-content" style="<?= $featuredImageStyle ?>">
+		<div class="container">
+			<div class="row">
+				<div class="columns small-12 medium-10 medium-offset-1 space-100-bottom">
+					<div class="title h3 space-min-bottom"><?= $thePost->get( 'post_title' ) ?></div>
+					<div class="row">
+						<div class="underline columns small-4 medium-3 large-2"><span class="fill-orange"></span></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Landing Section -->
+
+<!-- Post Section -->
+<section class="post-section space-50-top-bottom" id="post-content-section" style="min-height: 50vh;">
+	<div class="container">
+		<div class="row">
+			<div class="columns small-12 medium-10 medium-offset-1">
+				<div>
+					<!-- Share -->
+					<div class="block share-url js_share_url_widget">
+						<span class="share-url-title label text-uppercase inline-middle text-orange js_share_url_title">Share</span>
+						<i class="icon material-icons inline-middle text-orange">link</i>
+						<a class="share-url-label p inline-middle js_share_url" href="<?= $pageURL ?>" target="_blank"><?= $pageURL ?></a>
+						<textarea class="visuallyhidden js_share_url_text"><?= $pageURL ?></textarea>
+					</div>
+					<!-- END: Share -->
+					<?= $postContent ?: 'Um, what happened to the content?' ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END: Post Section -->
+
+<?php
+	// Program Booking section
+	$thePost->set( 'title', $thePost->get( 'post_title' ) );
+	$thePost->set( 'type', 'Travel' );
+	$programs = [ $thePost ];
+	BFS\UI\programBooking( $programs );
+?>
+
+
+
+
+
+<script type="text/javascript">
+
+	/*
+	 *
+	 * Share URLs
+	 *
+	 * When the share button or the URL is clicked/tapped
+	 * We copy the URL to the clipboard
+	 *
+	 */
+	$( document ).on( "click", ".js_share_url", function ( event ) {
+		event.preventDefault();
+	} );
+	$( document ).on( "click", ".js_share_url_widget", function ( event ) {
+
+		event.preventDefault();
+
+		var $shareURLWidget = $( event.target ).closest( ".js_share_url_widget" );
+		var $feedbackMessage = $shareURLWidget.find( ".js_share_url_title" );
+
+		var $url = $shareURLWidget.find( ".js_share_url_text" );
+		$url.get( 0 ).select();
+		try {
+			document.execCommand( "copy" );
+			$feedbackMessage.text( "Copied to Clipboard" );
+		}
+		catch ( e ) {}
+		$url.get( 0 ).blur();
+
+	} );
+
+</script>
+
+<?php require_once __ROOT__ . '/inc/footer.php'; ?>
